9c77ce26222d21bb92009d9f4d2bdb5eef92d412
eightyfive <benoit.sagols@gmail.com>
2020-09-03 18:09:26 +0700

    Comply with default datetimepicker behavior

--- datetimepicker.ios.js
+++ datetimepicker.ios.js
@@ -1,12 +1,24 @@
 import React from "react";
 import RNDateTimePicker from "@react-native-community/datetimepicker";
 
-import PickerModalIOS from "./picker-modal.ios";
+import ModalIOS from "./modal.ios";
 
-export default function DateTimePicker({ modal, ...rest }) {
-  if (modal === true) {
-    return <PickerModalIOS {...rest} Component={RNDateTimePicker} />;
+export default function DateTimePicker({
+  backgroundColor,
+  color,
+  modal = false,
+  onDismiss,
+  ...rest
+}) {
+  const picker = <RNDateTimePicker {...rest} />;
+
+  if (modal) {
+    return (
+      <ModalIOS {...{ color, backgroundColor, visible: true, onDismiss }}>
+        {picker}
+      </ModalIOS>
+    );
   }
 
-  return <RNDateTimePicker {...rest} />;
+  return picker;
 }
